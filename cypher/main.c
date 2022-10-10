ed748a5ea924be531caf33a4bebf739b134fbdf7
leejoonli <git@github.com>
2022-10-10 15:54:25 -0700

    adjust cypher

--- cypher/main.c
+++ cypher/main.c
@@ -2,60 +2,33 @@
 #include <stdlib.h>
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 
-int str_length(char arr[]) {
-    int i;
-    for(i = 0; arr[i] != '\0'; ++i);
-    return i;
-}
-
-int str_letter(char arr[], int arr_length) {
-    // check string for non letters
-    for(int i = 0; i < arr_length; i++) {
-        if(arr[i] == '\0' && i == 26) {
-            break;
-        }
-        else if(arr[i] < 65 || (arr[i] > 90 && arr[i] < 97) || arr[i] > 122) {
-            return 1;
-        }
+void cypher(char* plain, char* cypher) {
+    int i = 0;
+    while(plain[i] != '\0') {
+        cypher[i] = plain[i] + 1;
+        i++;
     }
-    // lower case string
-    for(int i = 0; i < arr_length; i++) {
-        if(arr[i] < 97) {
-            arr[i] = arr[i] + 32;
-        }
-    }
-    return 0;
+    cypher[i-1] = '\0';
+    return;
 }
 
 int main() {
     // variables
-    char key[27];
+    /*char key[27];*/
     char plain_text[256];
     char cypher_text[256];
-    int i = 0, j = ARRAY_SIZE(key);
+    int j = ARRAY_SIZE(plain_text);
 
-    // get cypher input
-    printf("Enter cypher key: \n");
-    fgets(key, j, stdin);
-
-    // check cypher input
-        // check length on input
-    if(str_length(key) != 26) {
-        printf("Cypher key needs to be 26 characters");
-        return 1;
-    }
-        // check if input has any char that isn't a letter in the alphabet
-    if(str_letter(key, j) == 1) {
-        printf("Cypher key must contain only letters\n");
-        return 1;
-    }
-
-    // get user text
-    printf("Enter phrase you wanted encrypted: ");
-    fgets(plain_text, 255, stdin);
+    // get user string input
+    printf("Enter text you wish to be encoded: \n");
+    fgets(plain_text, j, stdin);
 
     // cipher user text
+    cypher(plain_text, cypher_text);
 
     // output plain text and cipher text
+    printf("Plain text: %s", plain_text);
+    printf("Cypher text: %s", cypher_text);
+
     return 0;
 }
