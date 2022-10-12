cbe46272dd46361acf0ff564f190fddad5dddfe3
leejoonli <git@github.com>
2022-10-12 16:27:15 -0700

    complete mario

--- mario/main.c
+++ mario/main.c
@@ -3,6 +3,27 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int height;
+    printf("Enter height: ");
+    scanf("%d", &height);
+    if(height > 8) {
+        printf("height must not exceed 8");
+        return 1;
+    }
+    for(int i = 0; i < height; i++) {
+        for(int j = height - 1; j > i; j--) {
+            printf(" ");
+        }
+        for(int j = -1; j < i; j++) {
+            printf("#");
+        }
+        for(int k = 0; k < 2; k++) {
+            printf(" ");
+        }
+        for(int l = 0; l <= i; l++) {
+            printf("#");
+        }
+        printf("\n");
+    }
     return 0;
 }
