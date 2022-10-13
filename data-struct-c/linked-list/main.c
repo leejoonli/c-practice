f7e2276acd3624ccaab7a577108ac9f0808d486c
leejoonli <git@github.com>
2022-10-12 20:45:49 -0700

    add code

--- data-struct-c/linked-list/main.c
+++ data-struct-c/linked-list/main.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+struct node {
+    int key;
+    int data;
+    struct node *next;
+};
+
+struct node *head = NULL;
+
+void print_list() {
+   struct node *walker = head;
+   printf("\n[ ");
+
+   while(walker != NULL) {
+      printf("(%d,%d) ",walker->key,walker->data);
+      walker = walker->next;
+   }
+
+   printf(" ]");
+}
+
+void insert_first(int key, int data) {
+    struct node *new_node = (struct node*)malloc(sizeof(struct node));
+
+    new_node->key = key;
+    new_node->data = data;
+
+    new_node->next = head;
+    head = new_node;
+}
+
+int main() {
+    insert_first(2, 10);
+    insert_first(1, 10);
+    print_list();
     return 0;
 }
