8a5c40f97437d7f9f565703461a1debc46f4a135
leejoonli <git@github.com>
2022-10-19 16:12:01 -0700

    init stack exercise

--- data-struct-c/stack/main.c
+++ data-struct-c/stack/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-int main()
-{
-    printf("Hello world!\n");
+struct stack {
+    int top;
+    unsigned capacity;
+    int* array;
+};
+
+struct stack* stack_init(unsigned capacity) {
+    struct stack* stack = (struct stack*)malloc(sizeof(struct stack));
+    stack->capacity = capacity;
+    stack->top = -1;
+    stack->array = (int*)malloc(stack->capacity * sizeof(int));
+    return stack;
+}
+
+int main() {
+    struct stack* stack = stack_init(10);
+    printf("%p", stack);
     return 0;
 }
