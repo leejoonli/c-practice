e43dc9dcdca32fc6d9d0d08c5f6217420c74c9fc
leejoonli <git@github.com>
2022-10-20 16:02:45 -0700

    add create queue function

--- data-struct-c/queue/main.c
+++ data-struct-c/queue/main.c
@@ -8,6 +8,17 @@ struct queue {
 }
 
 // create queue
+struct queue* queue_init(unsigned capacity) {
+    struct queue* queue = (struct queue*)malloc(sizeof(struct queue));
+    queue->capacity = capacity;
+    queue->size = 0;
+    queue->front = queue->size;
+
+    queue->rear = capacity -1;
+    queue->array = (int*)malloc(queue->capacity * sizeof(int));
+    return queue;
+}
+
 // check queue full
 // check queue empty
 // enqueue
