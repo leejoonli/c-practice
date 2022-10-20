df64b840be31b4466c3af5062c8beefbfa839860
leejoonli <git@github.com>
2022-10-20 15:59:05 -0700

    initial commit for queue

--- data-struct-c/queue/main.c
+++ data-struct-c/queue/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct queue {
+    int front, rear, size;
+    unsigned capacity;
+    int* array;
+}
+
+// create queue
+// check queue full
+// check queue empty
+// enqueue
+// dequeue
+// get front
+// get rear
+// get size
+
 int main()
 {
     printf("Hello world!\n");
