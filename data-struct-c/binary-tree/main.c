dea9957f3560db22e6b3c5d4fd162936458f9e6f
leejoonli <git@github.com>
2022-10-21 14:37:09 -0700

    add new node function

--- data-struct-c/binary-tree/main.c
+++ data-struct-c/binary-tree/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct node {
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+// new node
+struct node* new_node(int data) {
+    struct node* node = (struct node*)malloc(sizeof(struct node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+// add left
+// add right
+
 int main()
 {
-    printf("Hello world!\n");
+    struct node* test = new_node(1);
+    printf("%i", test->data);
     return 0;
 }
