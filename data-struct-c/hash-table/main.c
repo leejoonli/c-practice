75be77eb2433577bc5d8ad4b8ed74b1a3d01d51e
leejoonli <git@github.com>
2022-11-02 16:40:27 -0700

    add search and delete functions

--- data-struct-c/hash-table/main.c
+++ data-struct-c/hash-table/main.c
@@ -16,12 +16,44 @@ void insert(struct item* hash_table[], int key, int data) {
     struct item* item = (struct item*)malloc(sizeof(struct item));
     item->key = key;
     item->data = data;
-
     int hash_index = hash(key);
 
+    while(hash_table[hash_index] != NULL && hash_table[hash_index]->key != -1) {
+        hash_index++;
+        hash_index %= SIZE;
+    }
+
     hash_table[hash_index] = item;
-    printf("%i, %i\n", hash_table[hash_index]->key, hash_table[hash_index]->data);
     return;
+}
+
+struct item* search(struct item* hash_table[], int key) {
+    int hash_index = hash(key);
+
+    while(hash_table[hash_index] != NULL) {
+        if(hash_table[hash_index]->key == key) {
+            return hash_table[hash_index];
+        }
+        hash_index++;
+        hash_index %= SIZE;
+    }
+    return NULL;
+}
+
+void delete(struct item* hash_table[], struct item* dummy_item, int key) {
+    struct item* item = search(hash_table, key);
+    int hash_index = hash(key);
+
+    while(hash_table[hash_index] != NULL) {
+        if(hash_table[hash_index]->key == item->key) {
+            struct item* temp = hash_table[hash_index];
+            hash_table[hash_index] = dummy_item;
+            return temp;
+        }
+        hash_index++;
+        hash_index %= SIZE;
+    }
+    return NULL;
 }
 
 int main()
@@ -31,6 +63,6 @@ int main()
     dummy_item->key = NULL;
     dummy_item->data = NULL;
     insert(hash_table, 1, 20);
-    printf("%i, %i", hash_table[1]->key, hash_table[1]->data);
+    delete(hash_table, dummy_item, 1);
     return 0;
 }
