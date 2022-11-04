d12a7d129781b332d738c481cf5b34bdc091c9ae
leejoonli <git@github.com>
2022-11-03 19:50:57 -0700

    add structs for graph

--- data-struct-c/graphs/main.c
+++ data-struct-c/graphs/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct adj_list_node {
+    int dest;
+    struct adj_list_node* next;
+};
+
+struct adj_list {
+    struct adj_list_node* head;
+};
+
+struct graph {
+    int i;
+    struct adj_list* array;
+};
+
 int main()
 {
     printf("Hello world!\n");
